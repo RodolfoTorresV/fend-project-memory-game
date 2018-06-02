a65fae04896e923816100e1257e735555aa5412c
Rodolfo Torres Vega <fito.torres94@gmail.com>
2018-06-01 19:06:14 -0500

    Basic Functionality
    The game shuffles 'cards' upon loading DOM content. Clicking card will 'flip'
    and show icon.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
-/*
- * Create a list that holds all of your cards
+ /*Look up (Array.from) method/prop for making an actual array from html elements.
+
+ Array.from(document.getElementsByClassName("events")).forEach(function(item) {
+   console.log(item.id);
+   SOURCE = https://stackoverflow.com/questions/22754315/for-loop-for-htmlcollection-elements
+});
+
  */
+document.addEventListener('DOMContentLoaded', function(){
+	newGame();
+});
 
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
+const allCards = document.getElementsByClassName('card');//returns an HTML collection, can't loop over this for some reason
+const arrayOfCards = [...allCards];//turns HTML collection into an actual array, 'loopable'
+const cardSet = document.querySelector('.deck');
+const flip = function(){
+	this.classList.toggle("open");
+	this.classList.toggle("show");
+}
+function shuffle(array) {// Shuffle function from http://stackoverflow.com/a/2450976
     var currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
@@ -24,6 +30,25 @@ function shuffle(array) {
 
     return array;
 }
+function newGame() {//WHAT?! THIS ACTUALLY WORKS!! IT SHUFFLES!!!!!
+	let newDeck = shuffle(arrayOfCards);
+	for(let x of newDeck){
+      [].forEach.call(newDeck, function(shuffled){
+         cardSet.appendChild(shuffled);
+  });
+  }
+};
+
+for(let theCard of arrayOfCards){
+	theCard.addEventListener('click', flip);//THE FREAKIN' CARDS FLIP NOW!!!!!! :'D
+};
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
 
 
 /*
